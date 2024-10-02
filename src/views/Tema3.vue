3dd9d3b9d47655f01c7f210afbef59fb08e5eb9c
lizmanchego <karinamanchego0416@gmail.com>
2024-10-02 00:21:27 -0500

    [MAQ] Tema 3 Full

--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -5,66 +5,210 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 3
-      h1 Imagen y apariencia personal (imagen profesional)
-    .row.justify-content-center.align-items-center.mb-4
-      .col-sm-12.col-lg-1.d-none.d-lg-block
+      h1 Trazabilidad del servicio al cliente
+    .row.justify-content-center.items-items-center.mb-5
+      .col-sm-12.col-md-8.col-lg-4.bg14.d-flex.justify-content-center.items-items-center.px-4.py-cus.mb-cus
         img(src='@/assets/curso/temas/tema3/img1.svg', alt='')
-      .col-sm-12.col-lg-11.bg08.p-4.mb-0
-        p.mb-0 La imagen y la apariencia personal es el pasaporte para ingresar al mundo social y laboral. Intervienen factores como: la forma de vestir, el peinado, el lenguaje no verbal, la manera de caminar, el comportamiento y el modo en que se llevan a cabo las interrelaciones con los demás.
-    p.mb-5 La imagen interna (valores, principios, creencias, conocimientos) debe estar acorde con la imagen externa (apariencia física, lenguaje corporal, presentación personal), que se transmite. 
-    .bgimg3.mb-3
-      h4.pt-1 Consejos para mejorar la imagen corporal
-    .row.justify-content-center.mb-5
-      .col-sm-12.col-lg-7 
-        LineaTiempoD.color-primario
-          .row(numero="1" titulo="Cuidar la higiene")
-            .col-sm-12.col-lg-12
-              p.mb-0 Para conservar una imagen sana y saludable. 
-          .row(numero="2" titulo="Seleccionar ropa cómoda")
-            .col-sm-12.col-lg-12
-              p.mb-0 De corte clásico, al igual que el calzado y los demás accesorios, elegantes según la personalidad, ocasión y preferencias. 
-          .row(numero="3" titulo="Buenos modales")
-            .col-sm-12.col-lg-12
-              p.mb-4 Tener presente siempre que los buenos modales y las normas de cortesía contribuyen al reconocimiento de la empresa y a la generación de sentido de pertenencia entre sus colaboradores: 
-              ul.lista-ul--color
-                li.d-flex
-                  i.fas.fa-phone-alt
-                  span #[b La uniformidad según la identidad corporativa y su normativa.] Normalmente predomina el gusto clásico, tradicional y universalmente aceptado (traje de chaqueta oscuro, camisa clara, zapato negro o azul según el color del traje, corbata para los hombres y pañuelo para las mujeres).
-                li.d-flex
-                  i.fas.fa-phone-alt
-                  span #[b Arreglo facial: maquillaje para las mujeres, suave y discreto.] En el caso de los hombres el rostro perfectamente afeitado. 
-                li.d-flex
-                  i.fas.fa-phone-alt
-                  span #[b Pulcritud corporal.] 
-                li.d-flex
-                  i.fas.fa-phone-alt
-                  span #[b Cabello limpio y retirado siempre del rostro,] con un corte adecuado a la edad y el trabajo. 
-                li.d-flex
-                  i.fas.fa-phone-alt
-                  span #[b Accesorios: de acuerdo con la normatividad de la empresa.] Usarlo de manera sobria, para conservar la elegancia. 
+      .col-sm-12.col-lg-8
+        p.mb-4 Se concibe la trazabilidad como todas y cada una de las acciones estandarizadas, que facilitan efectuar un registro histórico del recorrido y vida de un producto o servicio “a lo largo de la cadena de suministros en un momento dado, a través de unas herramientas determinadas”. (Comité de Seguridad Alimentaria de AECOC, 2009, p. 5) 
+        .cajon.color-primario.p-4.mb-4
+          h5 De acuerdo con Codex Alimentarios (2009), 
+          p.mb-0 Trazabilidad es “la capacidad para seguir el movimiento de un alimento a través de etapa(s) especificada(s) de la producción, transformación y distribución”. 
 
-          .row(numero="4" titulo="La sonrisa")
-            .col-sm-12.col-lg-12
-              p.mb-0 Una sonrisa puede abrir puertas. La sonrisa contagia a los clientes de alegría, positivismo, proyecta un ambiente laboral de amabilidad y buen trato. Es la antesala para iniciar una comunicación y también el toque agradable para finalizarla. Una sonrisa transmite comprensión, aceptación, complacencia. Para garantizar una hermosa sonrisa, se debe estar pendiente de una buena higiene oral. 
-          .row(numero="5" titulo="La imagen en sus manos")
-            .col-sm-12.col-lg-12
-              p.mb-3 Las manos son otro elemento del cuerpo, es la conexión con la piel del otro que puede comunicar cariño, respeto, familiaridad, aprecio, acogida, confianza. Las manos acompañan las palabras, se emplean para enfatizar, pero puede llegar a hacerse una lectura de autoritarismo, cuando se exagera en su uso y en la intensidad de los movimientos. 
-              p.mb-0 Una parte especial de las manos son las uñas, que deben cuidarse, pues ellas están en contacto visual directo con los clientes y pueden reflejar descuido. Lo recomendable es que las uñas se mantengan cortas, limpias y limadas, si las va a llevar pintadas emplee colores claros. 
+        p.mb-0 La trazabilidad entonces se entiende como los procedimientos definidos y estandarizados que permiten conocer la identificación, historia, localización y el destino final que ha tenido un producto o servicio a lo largo de la cadena de suministros y abastecimiento en un momento determinado. Es decir, es la serie de actividades establecidas con el fin de poder registrar el seguimiento a un producto o servicio hasta su destino final.
 
-          .row(numero="6" titulo="Uso de los perfumes y los desodorantes ")
-            .col-sm-12.col-lg-12
-              p.mb-0 Tanto para hombres como mujeres, se recomienda utilizar durante el día olores discretos, que destaquen su elegancia. Los olores conforman el imperceptible lenguaje de las fragancias. 
-          .row(numero="7" titulo="Vestuario")
-            .col-sm-12.col-lg-12
-              p.mb-3 Para las mujeres las faldas, en su largo, no debe sobrepasar los 6 centímetros por encima de la rodilla, no llevarlas demasiado ceñidas y la tela en que se confeccionen estas, así como pantalones, blusas y vestidos, no debe transparentar la ropa interior. Las blusas, si son transparentes, se deben llevar con “camisilla” debajo, tenga en cuenta además que no deben ser muy escotadas y sin mangas.
-              p.mb-3 Para los caballeros debe tener igual cuidado como el de la mujer. Así mismo deben cuidar el largo de los pantalones con el estilo de zapato, cuidar que las medias combinen con el color del pantalón, el color de la correa o cinturón combine con los zapatos y que los puños y cuello de la camisa estén impecables. 
-              p.mb-0 Finalmente, si usted usa uniformes asignados por la empresa, consérvelos en buen estado. Recuerde que la primera impresión que captan los clientes perdura en el tiempo y es de fácil evocación.
+    .row.justify-content-center.items-content-center.mb-5
+      .col-sm-12.col-lg-7
+        p.mb-4 El concepto surge como una necesidad de garantizar la calidad de los productos de la industria alimenticia, pero es aplicable a cualquier producto o servicio, en tanto que se busca identificar puntos críticos que pueden afectar un producto o servicio, con el fin de plantear acciones de mejoramiento, encaminado a alcanzar la satisfacción plena del cliente. 
+
+        .cajon.color-primario.p-4.mb-4
+          p.mb-0 La trazabilidad además permite brindar información completa al cliente, con el fin de incrementar su confianza y fidelidad. Además, fomenta en las empresas el sistema de autocontrol y autoevaluación que le permitirán mayor posicionamiento en los mercados.
+
       .col-sm-12.col-lg-5.d-none.d-lg-block
-          img(src='@/assets/curso/temas/tema3/img3.png', alt='')
+        img(src='@/assets/curso/temas/tema3/img5.png', alt='')
 
 
+    Separador
+    #t_3_1.titulo-segundo.color-acento-contenido
+      h2 3.1 Clases de trazabilidad
+    p.mb-5 La trazabilidad se clasifica teniendo en cuenta los procesos que desarrolla como respuesta a su actividad comercial, estas pueden ser: 
+    SlyderF.mb-5(columnas="col-md-6 col-lg-4 col-xl-4")
+      .tarjeta.slyder-f-cus.bgimg1.p-4
+        h4.py-1.text-center.bg04.mb-4.m-auto.px-3 Trazabilidad hacia atrás
+        p.text-center Si su negocio es ser proveedor de productos o servicios. 
+      .tarjeta.slyder-f-cus.bgimg1.p-4
+        h4.py-1.text-center.bg04.mb-4.m-auto.px-3 Trazabilidad interna o de proceso
+        p.text-center Revisión interna de los procesos que sigue para brindar un producto o servicio. 
+      .tarjeta.slyder-f-cus.bgimg1.p-4
+        h4.py-1.text-center.bg04.mb-4.m-auto.px-3 Trazabilidad hacia delante: 
+        p.text-center Si su negocio es entregar producto o servicio directamente al cliente. 
+      .tarjeta.slyder-f-cus.bgimg1.p-4
+        h4.py-1.text-center.bg04.mb-4.m-auto.px-3 Trazabilidad individual
+        p.text-center El término se aplica a la identificación individual del proceso, pero se puede aplicar también al seguimiento que puede realizar cada colaborador de la empresa de la labor que lleva a cabo, con el fin de efectuar una autoevaluación de su contribución con los objetivos y misión de la empresa.  
+
+
+
+
+    Separador
+    #t_3_2.titulo-segundo.color-acento-contenido
+      h2 3.2 Momento de la trazabilidad en el sistema
+    .row.justify-content-center.items-content-center.mb-5
+      .col-sm-12.col-lg-7
+        p.mb-4 Toda organización debe aplicar la trazabilidad como estrategia para mejorar sus procesos y gestión de servicios al cliente, es decir empresas de producción primaria como proveedores de productos, empresas administradoras de medicamentos, empresas de alimentos, empresas agropecuarias, empresas de distribución y ventas, empresas de minería y petróleos, empresas de servicios, entre otros sectores. 
+        p.mb-0 Al realizar una trazabilidad en las empresas se maximiza la seguridad y por ende se reciben beneficios tales como: confianza por parte del consumidor y mayor eficacia en gestión de incidencias para la administración; los cuales se reflejan en ganancias y ahorro económicos para la empresa.
+
+      .col-sm-12.col-lg-5.d-none.d-lg-block
+        img(src='@/assets/curso/temas/tema3/img6.png', alt='')
+
+
+    Separador
+    #t_3_3.titulo-segundo.color-acento-contenido
+      h2 3.3 Beneficios de la trazabilidad
+    p.mb-5 El implementar la trazabilidad y realizar la revisión de la cadena de suministro y abastecimiento representan para la empresa grandes beneficios como:
+    .row.justify-content-center.align-items-center.mb-5
+      .col-sm-12.col-lg-4.pr-0-cus.d-none.d-lg-block.p-0.m-0.h-card-4
+        img(src='@/assets/curso/temas/tema3/img7.svg', alt='')
+
+      .col-sm-12.col-lg-6.bg07.p-4.align-content-center.h-card-4
+        ul.lista-ul--color.mb-0
+          li.d-flex
+            i.fas.fa-phone-alt
+            span Disminución de costos y de elementos almacenados. 
+          li.d-flex
+            i.fas.fa-phone-alt
+            span Cumplimiento de la política “justo a tiempo”. 
+          li.d-flex
+            i.fas.fa-phone-alt
+            span Mejoramiento en proceso de producción, venta y post venta. 
+          li.d-flex
+            i.fas.fa-phone-alt
+            span Identificación de las necesidades del cliente. 
+          li.d-flex
+            i.fas.fa-phone-alt
+            span Tipificación de nuevos mercados. 
+          li.d-flex
+            i.fas.fa-phone-alt
+            span Participación en el mercado globalizado.  
+    p.mb-5 La puesta en marcha de un sistema de trazabilidad, debe partir de la formulación de un plan de trazabilidad, que será un primer paso para que todos y cada uno de los funcionarios de la organización asimilen y comprendan la dinámica y los propósitos que se buscan, como son: 
+    .row.justify-content-center.mb-5
+      .col-sm-12.col-lg-9
+        TabsA.color-acento-botones.mb-5
+          .tarjeta.bg09.p-5(titulo="Reconocer")
+            p.mb-0 Reconocer el proceso de vida del producto o servicio. 
+
+          .tarjeta.bg09.p-5(titulo="Identificar")
+            p.mb-0 Identificar puntos críticos (mal servicio, producto defectuoso, inconformidad con el producto o servicio) que puedan causar detrimento para la empresa o para el cliente. 
+
+          .tarjeta.bg09.p-5(titulo="Contar")
+            p.mb-0 Contar con una base de datos que contenga información completa de los productos o servicios. 
+
+          .tarjeta.bg09.p-5(titulo="Implementar")
+            p.mb-0 Implementar sistemas de autocontrol y autoevaluación. 
+
+          .tarjeta.bg09.p-5(titulo="Formular")
+            p.mb-0 Formular acciones de mejora frente a quejas, reclamos y devoluciones.  
+
+      .col-sm-12.col-lg-3.d-none.d-lg-block
+        img(src='@/assets/curso/temas/tema3/img8.png', alt='')
+    
+    Separador
+    #t_3_4.titulo-segundo.color-acento-contenido
+      h2 3.4 Ventajas de la aplicación de la trazabilidad
+    p.mb-5 El implementar un sistema de trazabilidad trae ventajas para la empresa y para el cliente, entre estas: 
+    TabsB.color-acento-contenido.mb-5
+      .p-5(titulo="Para las empresas" :icono="require('@/assets/curso/temas/tema3/img9.svg')")
+        .row
+          .col-sm-12.col-lg-8
+            h4 Para las empresas
+            ul.lista-ul--color
+                li.d-flex
+                  i.fas.fa-phone-alt
+                  span Incremento de ganancias, reducción de pérdidas. 
+                li.d-flex
+                  i.fas.fa-phone-alt
+                  span Responder a las necesidades del cliente. 
+                li.d-flex
+                  i.fas.fa-phone-alt
+                  span Identificar acciones de mejora.
+                li.d-flex
+                  i.fas.fa-phone-alt
+                  span Detectar puntos críticos a mejorar. 
+                li.d-flex
+                  i.fas.fa-phone-alt
+                  span Evita demandas por defectos en productos o mal servicio. 
+                li.d-flex
+                  i.fas.fa-phone-alt
+                  span Facilitar acciones de control y seguimiento a los procesos y la gestión. 
+                li.d-flex
+                  i.fas.fa-phone-alt
+                  span Favorecer el aseguramiento de la calidad. 
+                li.d-flex
+                  i.fas.fa-phone-alt
+                  span Facilitar la toma de decisiones. 
+                li.d-flex
+                  i.fas.fa-phone-alt
+                  span Garantiza la respuesta rápida y oportuna ante los compromisos adquiridos por la organización. 
+                li.d-flex
+                  i.fas.fa-phone-alt
+                  span Prever la logística necesaria para brindar un buen producto o servicio. 
+                li.d-flex
+                  i.fas.fa-phone-alt
+                  span Aumentar la lealtad y fidelidad de los clientes. 
+                li.d-flex
+                  i.fas.fa-phone-alt
+                  span Cumplir con las garantías ofrecidas. 
+                li.d-flex
+                  i.fas.fa-phone-alt
+                  span Mejorar la competitividad de la empresa dentro del mercado global.  
+
+          .col-sm-12.col-lg-4.d-none.d-lg-block
+            img(src='@/assets/curso/temas/tema3/img11.svg', alt='')
+    
+      .p-5(titulo="Para el cliente" :icono="require('@/assets/curso/temas/tema3/img10.svg')")
+        .row
+          .col-sm-12.col-lg-8
+            h4 Para el cliente
+            ul.lista-ul--color
+                li.d-flex
+                  i.fas.fa-phone-alt
+                  span  Aumentar la confianza y la fidelidad. 
+                li.d-flex
+                  i.fas.fa-phone-alt
+                  span Obtener información completa del producto o servicio. 
+                li.d-flex
+                  i.fas.fa-phone-alt
+                  span Recibir respuesta rápida y oportuna ante dudas o reclamaciones. 
+                li.d-flex
+                  i.fas.fa-phone-alt
+                  span Conseguir el cumplimiento de garantías. 
+                li.d-flex
+                  i.fas.fa-phone-alt
+                  span Recibir una atención perfecta.  
+
+          .col-sm-12.col-lg-4.d-none.d-lg-block
+            img(src='@/assets/curso/temas/tema3/img12.svg', alt='')
  
-
+    Separador
+    #t_3_5.titulo-segundo.color-acento-contenido
+      h2 3.5 Herramientas para la trazabilidad
+    p.mb-5 Para documentar un sistema de trazabilidad y garantizar que los resultados, sean tenidos en cuenta para la toma de decisiones encaminada a generar acciones de mejoramiento para asegurar una excelente calidad en el producto o servicio, se hace necesario implementar:
+    .row.justify-content-center.mb-5
+      .col-sm-12.col-lg-3.d-none.d-lg-block
+        img(src='@/assets/curso/temas/tema3/img13.png', alt='')
+      .col-sm-12.col-lg-9
+        AcordionA.mb-0(tipo="a" clase-tarjeta="tarjeta tarjeta--orange")
+          .row(titulo="Un sistema de codificación o simbolización")
+            .col-sm-12.col-lg-12.pl-cus
+              p.mb-0 Para dar un orden medible, contable y organizacional que va a permitir el seguir del producto o servicio. 
+          .row(titulo="Procedimientos documentados")
+            .col-sm-12.col-lg-12.pl-cus
+              p.mb-0 Dirigidos a brindar claridad sobre las actividades que se llevan a cabo dentro de la organización. 
+          .row(titulo="Mapa de procesos")
+            .col-sm-12.col-lg-12.pl-cus
+              p.mb-0 Ofrecer información de manera gráfica de los procesos organizacionales. 
+          .row(titulo="Confrontación con el cliente")
+            .col-sm-12.col-lg-12.pl-cus
+              p.mb-0 Permite verificar el destino final del producto o servicio y grado de satisfacción del cliente.  
 
 </template>
 
